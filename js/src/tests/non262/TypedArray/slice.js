b94f93627b7d7633f565f8d6fc6dd11aa29cb146
Ms2ger <Ms2ger@gmail.com>
2024-10-04 18:01:26 +0200

    Replace some simple uses of deepEqual in non262 tests

--- js/src/tests/non262/TypedArray/slice.js
+++ js/src/tests/non262/TypedArray/slice.js
@@ -1,26 +1,30 @@
+/*---
+includes: [compareArray.js]
+---*/
+
 for (var constructor of anyTypedArrayConstructors) {
-    assertDeepEq(constructor.prototype.slice.length, 2);
+    assert.sameValue(constructor.prototype.slice.length, 2);
 
-    assertDeepEq(new constructor().slice(0), new constructor());
-    assertDeepEq(new constructor().slice(0, 4), new constructor());
-    assertDeepEq(new constructor(10).slice(0, 2), new constructor(2));
+    assert.compareArray(new constructor().slice(0), new constructor());
+    assert.compareArray(new constructor().slice(0, 4), new constructor());
+    assert.compareArray(new constructor(10).slice(0, 2), new constructor(2));
 
-    assertDeepEq(new constructor([1, 2]).slice(1), new constructor([2]));
-    assertDeepEq(new constructor([1, 2]).slice(0), new constructor([1, 2]));
-    assertDeepEq(new constructor([1, 2, 3]).slice(-1), new constructor([3]));
-    assertDeepEq(new constructor([1, 2, 3, 4]).slice(-3, -1), new constructor([2, 3]));
-    assertDeepEq(new constructor([.1, .2]).slice(0), new constructor([.1, .2]));
+    assert.compareArray(new constructor([1, 2]).slice(1), new constructor([2]));
+    assert.compareArray(new constructor([1, 2]).slice(0), new constructor([1, 2]));
+    assert.compareArray(new constructor([1, 2, 3]).slice(-1), new constructor([3]));
+    assert.compareArray(new constructor([1, 2, 3, 4]).slice(-3, -1), new constructor([2, 3]));
+    assert.compareArray(new constructor([.1, .2]).slice(0), new constructor([.1, .2]));
 
-    assertDeepEq(new constructor([1, 2]).slice(-3), new constructor([1, 2]));
-    assertDeepEq(new constructor([1, 2]).slice(0, -3), new constructor());
-    assertDeepEq(new constructor([1, 2]).slice(4), new constructor());
-    assertDeepEq(new constructor([1, 2]).slice(1, 5), new constructor([2]));
+    assert.compareArray(new constructor([1, 2]).slice(-3), new constructor([1, 2]));
+    assert.compareArray(new constructor([1, 2]).slice(0, -3), new constructor());
+    assert.compareArray(new constructor([1, 2]).slice(4), new constructor());
+    assert.compareArray(new constructor([1, 2]).slice(1, 5), new constructor([2]));
 
     // Called from other globals.
     if (typeof newGlobal === "function") {
         var slice = newGlobal()[constructor.name].prototype.slice;
-        assertDeepEq(slice.call(new constructor([3, 2, 1]), 1),
-                     new constructor([2, 1]));
+        assert.compareArray(slice.call(new constructor([3, 2, 1]), 1),
+                            new constructor([2, 1]));
     }
 
     // Throws if `this` isn't a TypedArray.
